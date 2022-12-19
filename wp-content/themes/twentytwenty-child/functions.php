b46b1c2fb885fc34efb4b9c407e0545c8d9ef517
Gabriel Corredor <galcoca18@gmail.com>
2022-12-19 14:48:05 -0500

    Part 3 - Users

--- wp-content/themes/twentytwenty-child/functions.php
+++ wp-content/themes/twentytwenty-child/functions.php
@@ -1,6 +1,9 @@
 <?php
 
-/* functions */
+/*--------------------------------------------------*/
+/*          BEGIN PART 2 STYLESHEET ENQUEUE         */
+
+/* FUNCTIONS */
 
 function parentStyles() 
 {
@@ -17,8 +20,54 @@ function parentStyles()
 	);
 }
 
-/* actions */
+/* ACTIONS */
 
 add_action( 'wp_enqueue_scripts', 'parentStyles' );
+
+/*           END PART 2 STYLESHEET ENQUEUE          */
+/*--------------------------------------------------*/
+
+/*--------------------------------------------------*/
+/*            BEGIN PART 3 USER CREATION            */
+
+/* FUNCTIONS */
+
+function userCreationFunction() {
+    $username = 'wp-test';
+    $email = 'gabriel.ctest@elementor.com';
+    //$email = 'wptest@elementor.com';
+    $password = '123456789';
+
+    $user_id = username_exists( $username );
+    if (!$user_id && email_exists($email) == false) {
+        $user_id = wp_create_user( $username, $password, $email );
+        if( !is_wp_error($user_id) ) {
+            $user = get_user_by( 'id', $user_id );
+            $user->set_role( 'editor' );
+        }
+    }
+}
+
+function adminBarDisableFor() {
+    if (is_user_logged_in()) :
+        $user = wp_get_current_user();
+        $response = false;
+        if ($user->user_login == 'wp-test') :
+            $response = true;        
+        endif;
+        return $response;
+    endif;
+}
+
+if ( adminBarDisableFor() ) :
+    add_filter('show_admin_bar', '__return_false');
+endif;
+
+/* ACTIONS */
+add_action('init', 'userCreationFunction');
+
+/*             END PART 3 USER CREATION             */
+/*--------------------------------------------------*/
+
 
 ?>
